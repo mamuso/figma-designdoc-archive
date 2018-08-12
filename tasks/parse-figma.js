69eec5a5ca06f3fd8996eae1f1af221013e6688a
Manuel Muñoz Solera <mamuso@mamuso.net>
2018-08-12 00:37:00 -0700

    getting the json from figma

--- tasks/parse-figma.js
+++ tasks/parse-figma.js
@@ -1,5 +1,25 @@
 const fs = require("fs");
+const https = require("https");
 
 // Reading config.json
-const config = fs.readFileSync("config.json");
-const jsonConfig = JSON.parse(config);
+const jsonConfig = JSON.parse(fs.readFileSync("config.json"));
+
+// Return json from Figma
+const requestDocument = id => {
+  const options = {
+    host: "api.figma.com",
+    path: `/v1/files/"${id}`,
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "X-FIGMA-TOKEN": jsonConfig.figmaToken
+    }
+  };
+
+  const req = https.request(options, function(res) {
+    res.setEncoding("utf8");
+    res.on("data", function(body) {});
+  });
+
+  req.end();
+};
