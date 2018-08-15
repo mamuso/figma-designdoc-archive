112a149eb43d079e8f05c72e3ab8268e28183ff2
Manuel Muñoz Solera <mamuso@mamuso.net>
2018-08-14 17:53:17 -0700

    Starting the construction of the markdowns

--- _plugins/figma-designdoc.rb
+++ _plugins/figma-designdoc.rb
@@ -5,14 +5,58 @@ require 'dotenv'
 
 class FigmaDesigndoc
   def initialize(site)
-    # load .env file
     Dotenv.load
 
     # set figma token. Values in the .env always take precedence
     @figmatoken = ENV['FIGMATOKEN'] ? ENV['FIGMATOKEN'] : site.config["figmaconfig"]["token"]
+    @figmadocuments = site.config["figmaconfig"]["documents"]
+    @figmascale = site.config["figmaconfig"]["scale"]
+  end
 
-    puts @figmatoken
+  def fetch
+    @figmadocuments.each do |doc|
 
+      content = JSON.parse self.getDoc(doc)
+      pages = content["document"]["children"]
+
+      # Let's generate a markdow per page
+      pages.each do |page|
+        self.writeMarkdown(page) if parseElement?(page["name"])
+      end
+
+    end
+  end
+
+  # write a markdow document for each page
+  def writeMarkdown(page)
+    layers = page["children"].reverse
+
+    # Do we have a preface?
+    preface = layers.select do |layer|
+      layer["type"] == "TEXT"
+    end
+    # preface["characters"]
+
+    # Let's get only the frames
+    frames = layers.select do |layer|
+      layer["type"] === "FRAME"
+    end
+    
+    # Let's get the text layers
+    text = layers.select do |layer|
+      layer["type"] === "TEXT"
+    end
+
+  end
+
+  # decidign if we should parse an element or not
+  def parseElement?(string)
+    return string.start_with?("~") ? false : true
+  end
+
+  # decidign if we should show the title above an element or not
+  def showTitle?(string)
+    return string.start_with?("_") ? false : true
   end
 
   # get the json of a given doc
@@ -28,12 +72,15 @@ class FigmaDesigndoc
     response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
       http.request(request)
     end
-    puts response.body
+    return response.body
   end
 
 end
 
 # Generate everything we need after initializing the site
 Jekyll::Hooks.register :site, :after_init do |site|
+
   figma = FigmaDesigndoc.new(site)
+  figma.fetch
+
 end
